e1f669206184335472df01502e471b0f4a4cefc6
JC <jcreal2829@gmail.com>
2026-06-21 18:38:38 +0100

    algumas  alterações

---
+++ site-de-teste/js/index.js
@@ -0,0 +1,5 @@
+console.log("5+5");
+document.getElementById("paragrafo-teste").innerHTML = "Este texto foi adicionado por <b>JavaScript</b>!";
+document.getElementById("paragrafo-teste").style.color = "blue";
+document.getElementById("paragrafo-teste").style.fontSize = "20px";
+alert("Bem-vindo ao meu site de teste!");
